8f84108c9d6fcdc6c2765a943a8e4ff3972843db
Ali <2021170338@cis.asu.edu.eg>
2025-03-12 00:02:44 +0200

    Add SeekerStats component and mock data for seeker statistics

--- src/pages/SeekerDashboard.tsx
+++ src/pages/SeekerDashboard.tsx
@@ -1,10 +1,11 @@
 import UserNav from "../components/Header/UserNav";
 import TabGroup from "../components/Tabs/TabGroup";
 import useStore from "../stores/globalStore";
-import SeekerJobsAppliedFor from "../components/SeekerJobsAppliedFor/SeekerJobsAppliedFor";
+import SeekerJobsAppliedFor from "../components/Seeker Dashboard/SeekerJobsAppliedFor";
 import SkeletonLoader from "../components/common/SkeletonLoader";
 import { useEffect } from "react";
 import { seekerDashboardTabs } from "../stores/Seeker Dashboard Slices/dashboardPageSlice";
+import SeekerStats from "../components/Seeker Dashboard/SeekerStats";
 
 const SeekerDashboard = () => {
     const activeTab = useStore.useSeekerDashboardActiveTab();
@@ -21,7 +22,7 @@ const SeekerDashboard = () => {
     return (
         <>
         <UserNav />
-        <div className="min-h-screen bg-gray-100 pt-1 px-4 pb-20 sm:px-6 lg:px-8 ">
+        <div className="min-h-screen bg-gray-100 pt-1 px-4 pb-20 sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto">
                 <TabGroup
                     tabs={["Jobs Applied For", "Assessments", "Interviews", "Offers"]}
@@ -30,6 +31,7 @@ const SeekerDashboard = () => {
                     useSetActiveTab={useSetActiveTab}
                 />
             </div>
+            <SeekerStats />
             {loadingTab !== null ? (
                 <SkeletonLoader />
             ) : activeTab == 0 ? (
