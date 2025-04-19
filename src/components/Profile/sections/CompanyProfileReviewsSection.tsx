6381d24fade75f7cd2177b122e7fd595072bb433
Omar Farouk <omarfarouk219@gmail.com>
2025-04-19 22:25:20 +0200

    Add reviews section to company profile
    Introduce a new component to display company reviews with filtering options
    Refactor review list props
    Improve styling and layout consistency in the review list

---
+++ src/components/Profile/sections/CompanyProfileReviewsSection.tsx
@@ -0,0 +1,50 @@
+import useStore from "../../../stores/globalStore";
+import { useEffect } from "react";
+import ReviewList from "../../Review/ReviewList";
+import FilterDropdown from "../../Filters/FilterDropdown";
+import { sortByDateOptions, reviewsRatingOptions } from "../../../data/filterOptions";
+import { mockReviews } from "../../../mock data/seekerProfile";
+
+export default function ReviewsSection() {
+  const reviews = mockReviews;
+  // const reviews = useStore.useCompanyProfileReviews();
+  const hasMore = useStore.useCompanyProfileReviewsHasMore();
+  const isLoading = useStore.useCompanyProfileReviewsIsLoading();
+  const fetchReviews = useStore.useCompanyProfileFetchReviews();
+  const filters = useStore.useCompanyProfileReviewsFilters();
+  const setFilters = useStore.useCompanyProfileSetReviewsFilters();
+
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  return (
+    <div className="p-6 bg-white rounded-3xl shadow border-2 border-gray-200">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Reviews</h1>
+        <div className="flex space-x-12">
+          <FilterDropdown
+            label="Sort By Date"
+            options={sortByDateOptions}
+            selectedValue={filters.sortByDate}
+            onSelect={(value) => setFilters({ sortByDate: value })}
+          />
+          <FilterDropdown
+            label="Job Title"
+            options={reviewsRatingOptions}
+            selectedValue={filters.rating}
+            onSelect={(value) => setFilters({ rating: value })}
+          />
+        </div>
+      </div>
+      <div className="mt-8">
+        <ReviewList
+          reviews={reviews}
+          hasMore={hasMore}
+          isLoading={isLoading}
+          onLoadMore={fetchReviews}
+        />
+      </div>
+    </div>
+  );
+}
