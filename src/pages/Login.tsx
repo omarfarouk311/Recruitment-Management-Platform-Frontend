1d6e4cf518f379ce96449c9a800798fb425a5122
Omar Farouk <omarfarouk219@gmail.com>
2025-06-11 01:50:29 +0300

    Refactor login and signup forms
    - Replace custom state-based form handling with react-hook-form and Zod schema validation in login and signup pages.
    - Update navigation flow and UI adjustments for better user experience.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,133 +1,161 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import AuthLayout from "../components/common/AuthLayout";
 import Input from "../components/common/Input";
 import Button from "../components/common/Button";
-import googleLogo from "../assets/google-logo.png";
-import facebookLogo from "../assets/facebook-logo.png";
 import axios from "axios";
-import config from '../../config/config';
+import config from "../../config/config";
 import useStore from "../stores/globalStore";
+import { showErrorToast } from "../util/errorHandler";
+import { UserRole } from "../stores/User Slices/userSlice";
+// import googleLogo from "../assets/google-logo.png";
+// import facebookLogo from "../assets/facebook-logo.png";
+
+const loginSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(1, "Password is required"),
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
 
 const Login = () => {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const setUserId=useStore.useUserSetId();
-  const setName=useStore.useUserSetName();
-  const setRole=useStore.useUserSetRole();
+    const navigate = useNavigate();
+    const setUserId = useStore.useUserSetId();
+    const setRole = useStore.useUserSetRole();
+    const setName = useStore.useUserSetName();
+    const setImage = useStore.useUserSetImage();
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<LoginFormData>({
+        resolver: zodResolver(loginSchema),
+    });
 
+    const onSubmit = async (data: LoginFormData) => {
+        try {
+            const res = await axios.post(
+                `${config.API_BASE_URL}/auth/login`,
+                { email: data.email, password: data.password },
+                { withCredentials: true }
+            );
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-    // Add login logic here
-    let res;
-    try {
-        res = await axios.post(`${config.API_BASE_URL}/auth/login`, {
-        email,
-        password
-      },
-      {
-        withCredentials: true 
-      }
+            const { userId, userRole } = res.data;
 
-    );
+            // Update global state with user data
+            setUserId(userId);
+            setRole(userRole);
 
-      setUserId(res.data.userId);
-      setName(res.data.name); 
-      setRole(res.data.userRole);
-      
-      // localStorage.setItem("token", res.data.token);
-      navigate("/seeker/home");
-    } catch (error: any) {
-      alert(error.response?.data?.message || "Login failed");
-    } finally {
-      setLoading(false);
-    }
-   
-  };
+            // If the user has not completed their profile, redirect to finish profile page
+            if (res.data.name === null) {
+                if (userRole === UserRole.SEEKER) {
+                    navigate("/seeker/finish-profile", { replace: true });
+                } else if (userRole === UserRole.COMPANY) {
+                    navigate("/company/finish-profile", { replace: true });
+                } else if (userRole === UserRole.RECRUITER) {
+                    navigate("/recruiter/finish-profile", { replace: true });
+                }
+            }
+            // the user has completed their profile
+            else {
+                setName(res.data.name);
+                if (userRole === UserRole.SEEKER) {
+                    setImage(`${config.API_BASE_URL}/seekers/profiles/${userId}/image?t=${Date.now()}`);
+                    navigate("/seeker/home", { replace: true });
+                } else if (userRole === UserRole.COMPANY) {
+                    setImage(`${config.API_BASE_URL}/companies/${userId}/image?t=${Date.now()}`);
+                    navigate("/company/dashboard", { replace: true });
+                } else if (userRole === UserRole.RECRUITER) {
+                    setImage(`${config.API_BASE_URL}/recruiters/${userId}/profile-pic?t=${Date.now()}`);
+                    navigate("/recruiter/dashboard", { replace: true });
+                }
+            }
+        } catch (error: any) {
+            if (axios.isAxiosError(error)) {
+                if (error.response?.status === 401) {
+                    showErrorToast("Invalid email or password");
+                } else if (error.response?.status === 400) {
+                    const validationErrors: string[] = error.response?.data.validationErrors;
+                    validationErrors.forEach((validationError) => {
+                        showErrorToast(validationError);
+                    });
+                } else {
+                    showErrorToast("Login failed. Please try again.");
+                }
+            }
+        }
+    };
 
-  return (
-    <AuthLayout
-      title="Log in"
-      subtitle="New to the platform?"
-      link={
-        <span
-          onClick={() => navigate("/signup")}
-          role="button"
-          className="text-purple-600 underline underline-offset-4 hover:text-purple-400 cursor-pointer transition-colors"
+    return (
+        <AuthLayout
+            title="Welcome back!"
+            subtitle="New to the platform?"
+            link={
+                <span
+                    onClick={() => navigate("/signup")}
+                    role="button"
+                    className="text-purple-600 underline underline-offset-4 hover:text-purple-400 cursor-pointer transition-colors"
+                >
+                    Create an account
+                </span>
+            }
         >
-          Sign up
-        </span>
-      }
-    >
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <Input
-        label="Email address"
-        type="email"
-        placeholder="Enter your email address"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+                <Input
+                    label="Email address"
+                    type="email"
+                    placeholder="Enter your email address"
+                    error={errors.email?.message}
+                    {...register("email")}
+                />
 
-      <Input
-        label="Password"
-        type="password"
-        placeholder="Enter your password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+                <Input
+                    label="Password"
+                    type="password"
+                    placeholder="Enter your password"
+                    error={errors.password?.message}
+                    {...register("password")}
+                />
 
-        <div className="flex items-center justify-end">
-          <Link
-            to="/forgot-password"
-            className="text-sm text-purple-600 hover:text-purple-400"
-          >
-            Forgot password?
-          </Link>
-        </div>
+                {/* <div className="flex items-center justify-end">
+                    <button
+                        type="button"
+                        onClick={() => navigate("/forgot-password")}
+                        className="text-sm text-purple-600 hover:text-purple-400"
+                    >
+                        Forgot password?
+                    </button>
+                </div> */}
 
-        <Button type="submit" loading={loading}>
-          Log in
-        </Button>
+                <Button type="submit" loading={isSubmitting} className="!h-10">
+                    Login
+                </Button>
 
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-200" />
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-white text-gray-500">
-              OR Continue with
-            </span>
-          </div>
-        </div>
+                {/* <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-200" />
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="px-2 bg-white text-gray-500">OR Continue with</span>
+                    </div>
+                </div>
 
-        <div className="grid grid-cols-2 gap-4">
-          <Button
-            variant="outline"
-            type="button"
-            className="flex items-center gap-2"
-          >
-            <img src={googleLogo} alt="Google logo" className="w-5 h-5" />
-            Google
-          </Button>
-          <Button
-            variant="outline"
-            type="button"
-            className="flex items-center gap-2"
-          >
-            <img src={facebookLogo} alt="Facebook logo" className="w-7 h-7" />
-            Facebook
-          </Button>
-        </div>
-      </form>
-    </AuthLayout>
-  );
+                <div className="grid grid-cols-2 gap-4">
+                    <Button variant="outline" type="button" className="flex items-center gap-2">
+                        <img src={googleLogo} alt="Google logo" className="w-5 h-5" />
+                        Google
+                    </Button>
+                    <Button variant="outline" type="button" className="flex items-center gap-2">
+                        <img src={facebookLogo} alt="Facebook logo" className="w-7 h-7" />
+                        Facebook
+                    </Button>
+                </div> */}
+            </form>
+        </AuthLayout>
+    );
 };
 
 export default Login;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,143 +1,141 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import AuthLayout from "../components/common/AuthLayout";
 import Input from "../components/common/Input";
 import Button from "../components/common/Button";
-import googleLogo from "../assets/google-logo.png";
-import facebookLogo from "../assets/facebook-logo.png";
 import axios from "axios";
 import config from "../../config/config";
 import useStore from "../stores/globalStore";
+import { showErrorToast } from "../util/errorHandler";
+// import googleLogo from "../assets/google-logo.png";
+// import facebookLogo from "../assets/facebook-logo.png";
+
+const signUpSchema = z
+    .object({
+        email: z.string().email("Invalid email address"),
+        password: z
+            .string()
+            .min(8, "Password must be at least 8 characters")
+            .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+            .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+            .regex(/[0-9]/, "Password must contain at least one number")
+            .regex(/[^a-zA-Z0-9]/, "Password must contain at least one special character"),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
+
+type SignUpFormData = z.infer<typeof signUpSchema>;
 
 const SignUp = () => {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const userRole=useStore.useUserRole();
+    const navigate = useNavigate();
+    const userRole = useStore.useUserRole();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SignUpFormData>({
+        resolver: zodResolver(signUpSchema),
+    });
 
-    if (password !== confirmPassword) {
-      alert("Passwords do not match.");
-      setLoading(false);
-      return;
-    }
+    const onSubmit = async (data: SignUpFormData) => {
+        try {
+            await axios.post(`${config.API_BASE_URL}/auth/signup`, {
+                email: data.email,
+                password: data.password,
+                confirmationPassword: data.confirmPassword,
+                role: userRole,
+            });
+            navigate("/login", { replace: true });
+        } catch (err: any) {
+            if (axios.isAxiosError(err)) {
+                if (err.response?.status === 400) {
+                    const validationErrors: string[] = err.response?.data.validationErrors;
+                    validationErrors.forEach((validationError) => {
+                        showErrorToast(validationError);
+                    });
+                } else if (err.response?.status === 409) {
+                    showErrorToast(err.response?.data.message);
+                } else {
+                    showErrorToast("Failed to create account. Please try again.");
+                }
+            }
+        }
+    };
 
-    try {
-
-      const res = await axios.post(`${config.API_BASE_URL}/auth/signup`, {
-        email,
-        password,
-        confirmationPassword:confirmPassword,
-        role:userRole
-      }); 
-      if(userRole===2){
-        navigate("/recruiter/profile");
-      }
-      else if(userRole===0){
-        navigate("/seeker/profile");
-      }
-      else{
-        navigate("/company/profile");
-      }
-    } catch (error: any) {
-      alert(error.response?.data?.message || "Signup failed");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return (
-    <AuthLayout
-      title="Create an account"
-      subtitle="Already have an account?"
-      link={
-        <span
-          onClick={() => navigate("/login")}
-          role="button"
-          className="text-purple-600 underline underline-offset-4 hover:text-purple-400 cursor-pointer transition-colors"
+    return (
+        <AuthLayout
+            title="Create an account"
+            subtitle="Already have an account?"
+            link={
+                <span
+                    onClick={() => navigate("/login")}
+                    role="button"
+                    className="text-purple-600 underline underline-offset-4 hover:text-purple-400 cursor-pointer transition-colors"
+                >
+                    Login
+                </span>
+            }
         >
-          Log in
-        </span>
-      }
-    >
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <Input
-          label="What should we call you?"
-          placeholder="Enter your profile name"
-          required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          label="What's your email?"
-          type="email"
-          placeholder="Enter your email address"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          label="Create a password"
-          type="password"
-          placeholder="Enter your password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Input
-          label="Confirm your password"
-          type="password"
-          placeholder="Enter your confirm password"
-          required
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <p className="text-sm text-gray-500">
-          Use 8 or more characters with a mix of letters, numbers & symbols
-        </p>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+                <Input
+                    label="What's your email?"
+                    placeholder="Enter your email address"
+                    error={errors.email?.message}
+                    {...register("email")}
+                />
 
-        <Button type="submit" loading={loading}>
-          Create an account
-        </Button>
+                <Input
+                    label="Create a password"
+                    type="password"
+                    placeholder="Enter your password"
+                    error={errors.password?.message}
+                    {...register("password")}
+                />
 
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-200" />
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-white text-gray-500">
-              OR Continue with
-            </span>
-          </div>
-        </div>
+                <Input
+                    label="Confirm your password"
+                    type="password"
+                    placeholder="Enter your confirm password"
+                    error={errors.confirmPassword?.message}
+                    {...register("confirmPassword")}
+                />
 
-        <div className="grid grid-cols-2 gap-4">
-          <Button
-            variant="outline"
-            type="button"
-            className="flex items-center gap-2"
-          >
-            <img src={googleLogo} alt="Google logo" className="w-6 h-6" />
-            Google
-          </Button>
-          <Button
-            variant="outline"
-            type="button"
-            className="flex items-center gap-2"
-          >
-            <img src={facebookLogo} alt="Facebook logo" className="w-8 h-8" />
-            Facebook
-          </Button>
-        </div>
-      </form>
-    </AuthLayout>
-  );
+                <p className="text-sm text-gray-500">
+                    Use 8 or more characters with a mix of letters, numbers & symbols
+                </p>
+
+                <Button type="submit" loading={isSubmitting} className="!h-10">
+                    Create an account
+                </Button>
+
+                {/* <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-200" />
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="px-2 bg-white text-gray-500">OR Continue with</span>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4">
+                    <Button variant="outline" type="button" className="flex items-center gap-2">
+                        <img src={googleLogo} alt="Google logo" className="w-6 h-6" />
+                        Google
+                    </Button>
+                    <Button variant="outline" type="button" className="flex items-center gap-2">
+                        <img src={facebookLogo} alt="Facebook logo" className="w-8 h-8" />
+                        Facebook
+                    </Button>
+                </div> */}
+            </form>
+        </AuthLayout>
+    );
 };
 
 export default SignUp;
